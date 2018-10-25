5c50b0bc09fb7e4016791af7afe4f57d2088ffad
snlo <snloveydus@sina.com>
2018-10-25 18:00:38 +0800

    第一次提交

---
+++ SNTabBarController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SNTabBarController
+//
+//  Created by snlo on 2018/10/25.
+//  Copyright © 2018 snlo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SNTabBarController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SNTabBarController
+//
+//  Created by snlo on 2018/10/25.
+//  Copyright © 2018 snlo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
